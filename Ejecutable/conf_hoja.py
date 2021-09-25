c5e2b2a00c76c9d21c42f0014f4fece0c4d67b5b
PedroRomagnano <pedroroma0610@gmail.com>
2021-09-25 20:21:37 -0300

    Fondo y correcciones Front End

--- Ejecutable/conf_hoja.py
+++ Ejecutable/conf_hoja.py
@@ -1,25 +1,32 @@
+import tkinter
 import xlsxwriter
 from tkinter import *
 from tkinter.ttk import Combobox
+import tkinter.messagebox
+from tkinter import filedialog
 from pathlib import Path
 from xlsxwriter import *
 from functools import partial
+import tkinter.font as font
+from tkinter import ttk
  
-workbook = xlsxwriter.Workbook('Prueba.xlsx') #agregar path
+workbook = xlsxwriter.Workbook('C:/Users/Pedri/Desktop/Prueba.xlsx') 
 worksheet = workbook.add_worksheet()
 window = Tk()
 window.title('Configuacion de hoja')
 window.geometry('300x200+10+20')
-
+window.configure(background='#ADD8E6')
+window.iconbitmap('C:/Users/Pedri/Desktop/Zurich/DLR/Iconos/icono.ico')
+myFont = font.Font(family='Nunito')
 
 # CREO LOS LABELS QUE VA A TENER A LA IZQUIERDA DEL DROPBOX
-Hoja = Label(window,text='Tamaño de hoja: ', font=('Calibri',12))
+Hoja = Label(window,text='Tamaño de hoja: ', font=(myFont,11), bg='#ADD8E6')
 Hoja.place(x = 20, y = 20)
-Letra = Label(window, text = 'Tamaño de letra: ',font=('Calibri',12) )
+Letra = Label(window, text = 'Tamaño de letra: ',font=(myFont,11),bg='#ADD8E6' )
 Letra.place(x = 20, y = 50)
-Vorientacion = Label(window, text='Orientacion vertical:',font=('Calibri',12))
+Vorientacion = Label(window, text='Orientacion vertical:',font=(myFont,11), bg='#ADD8E6')
 Vorientacion.place(x=20,y=80)
-Horientacion = Label(window, text='Orientacion horizontal:',font=('Calibri',12))
+Horientacion = Label(window, text='Orientacion horizontal:',font=(myFont,11), bg='#ADD8E6')
 Horientacion.place(x=20,y=110)
 
 # CREO EL DROPBOX PARA LA HOJA
@@ -28,10 +35,11 @@ lbformato = Combobox(window,values = formatoHoja, width=3)
 lbformato.place(x=180,y=23)
 
 # TEXT FIELD PARA LA LETRA 
-NumLetra = Entry(window, text="14",width=3) 
+NumLetra = Entry(window, text = '14' ,width=4) 
 NumLetra.place(x=180, y=53)
 
-# DROPBOX PARA ORIENTACIONES
+
+# DROPBOX PARA ORIENTACIONES DE TEXTO
 Overt = ['Arriba', 'Centro', 'Abajo']
 lbOvert = Combobox(window, values=Overt, width = 8)
 lbOvert.place(x=180,y=83)
@@ -42,20 +50,34 @@ lbOhori.place(x=180,y=113)
 
 # BOTON DE GUARDADO  
 def guardar (): # guarda en la lista y cierra la ventana
-    thoja = lbformato.get()
-    tletra = NumLetra.get()
-    orivert = lbOvert.get()
-    orihor = lbOhori.get()
 
-    window.destroy() # agregar pop up guardado exitosamente mbox.show 
+    thoja = lbformato.get() # Tipo de hoja
+    #tletra = NumLetra.get() # tamanio de letra
+    orivert = lbOvert.get() # orientacion vertical
+    orihor = lbOhori.get() # orientacion horizontal
+
+    def solonum():
+        try:
+            float(NumLetra.get())
+        except ValueError:
+            tkinter.messagebox.showerror('Error', 'Ingrese un numero')
+        return
+
+    tkinter.messagebox.showinfo('Guarado', 'La configuracion ha sido guardada correctamente')
+    window.destroy() # cierra la ventana y guarda
+
+
 
     if thoja == 'A4':
         worksheet.set_paper(9)
     elif thoja == 'A5':
         worksheet.set_paper(11)
-    
-savebtn = Button(window, text = 'Guardar', command=guardar)
-savebtn.place(x= 220,y=160)
+
+pngGuardar = PhotoImage(file='C:/Users/Pedri/Desktop/Zurich/DLR/Iconos/discket.png')
+# Esto permite poner la imagen como boton
+savebtn = Button(window, image = pngGuardar, command=guardar, bg='#ADD8E6',activebackground= '#ADD8E6' , borderwidth=0)
+'''state=DISABLED Esto bloquea el boton, estaria bueno agregarlo cuando no hay nada tipeado''' 
+savebtn.place(x= 240,y=155)
 
 # MARGENES PARA LA HOJA
 worksheet.set_margins(left=0.0, right=0.0, top=0.0, bottom=0.0)
