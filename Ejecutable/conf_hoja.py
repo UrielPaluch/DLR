ed4630e398b80d48933134b4907cfd8c2d1137fd
PedroRomagnano <pedroroma0610@gmail.com>
2021-10-13 16:56:06 -0300

    Parte de receta

--- Ejecutable/conf_hoja.py
+++ Ejecutable/conf_hoja.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from xlsxwriter import *
 from functools import partial
 import tkinter.font as font
-from tkinter import ttk
+
  
 # PANTALLA DE INICIO VACIA
 root = Tk()
@@ -22,39 +22,55 @@ def abrir():
     # PANTALLA DE CONFIGURAICON
     ConfHoja = Toplevel()
     ConfHoja.title('Configuacion de hoja')
-    ConfHoja.geometry('700x300')
+    ConfHoja.geometry('800x300')
     ConfHoja.configure(background='#F08080') #F08080 COLOR SALMON
     ConfHoja.iconbitmap('C:/Users/Pedri/Desktop/Zurich/DLR/Iconos/icono.ico')
     ConfHoja.resizable(False,False)
     myFont = font.Font(family='Nunito')
 
-    # CREO LOS LABELS QUE VA A TENER A LA IZQUIERDA DEL DROPBOX
+    # CREO EL LABEL QUE VA A TENER A LA IZQUIERDA DEL DROPBOX PARA LA HOJA
+    # GENERAL
     labelHoja = Label(ConfHoja,text='Tamaño de hoja: ', font=(myFont,11), bg='#F08080')
     labelHoja.grid(column = 2, row= 2,padx= 5, pady = 5, sticky='w')
-    labelLetra = Label(ConfHoja, text = 'Tamaño de letra: ',font=(myFont,11),bg='#F08080' )
-    labelLetra.grid(column = 2, row= 4,padx= 5, pady = 5, sticky='w')
-    labelVori = Label(ConfHoja, text='Orientacion vertical:',font=(myFont,11), bg='#F08080')
-    labelVori.grid(column = 2, row= 6,padx= 5, pady = 5, sticky='w')
-    labelHori = Label(ConfHoja, text='Orientacion horizontal:',font=(myFont,11), bg='#F08080')
-    labelHori.grid(column = 2, row= 8,padx= 5, pady = 5, sticky='w')
-
-    # CREO EL DROPBOX PARA LA HOJA
+    labelTituloGeneral = Label(ConfHoja, text = 'Configuracion de campos', font= (myFont,16), bg ='#F08080')
+    labelTituloGeneral.grid(column = 4 , row = 3, padx = 10, pady = 5)
+    # BOX PARA LA HOJA
     tipoHoja = ['A4', 'A5']
     boxtipoHoja = Combobox(ConfHoja,values = tipoHoja, width=3)
     boxtipoHoja.grid(column = 3, row= 2,padx= 5, pady = 5, sticky='w')
+    #Canvas.create_line(15, 25, 200, 25) # para hacer una linea abajo del titulo
 
-    # TEXT FIELD PARA LA LETRA 
-    tmnLetra = Entry(ConfHoja, text = '14' ,width=4) 
-    tmnLetra.grid(column = 3, row= 4,padx= 5, pady = 5, sticky='w')
+    # LABELS NUMERO DE RECETA 
+    labelTitulo_receta = Label(ConfHoja,text = 'Numero de receta', font=(myFont,14), bg='#F08080')
+    labelTitulo_receta.grid(column = 2,row = 4,padx= 5, pady = 5, sticky='w')
 
-    # DROPBOX PARA ORIENTACIONES VERTICAL Y HORIZONTAL DE TEXTO
-    ori_vert = ['Arriba', 'Centro', 'Abajo']
-    boxOri_vert = Combobox(ConfHoja, values=ori_vert, width = 8)
-    boxOri_vert.grid(column = 3, row= 6,padx= 5, pady = 5, sticky='w')
+    labelLetra_receta = Label(ConfHoja, text = 'Tamaño de letra: ',font=(myFont,11),bg='#F08080' )
+    labelLetra_receta.grid(column = 2, row= 6,padx= 5, pady = 5, sticky='w')
 
-    ori_hori = ['Izquierda', 'Centro', 'Derecha']
-    boxOri_hori = Combobox(ConfHoja, values=ori_hori, width = 8)
-    boxOri_hori.grid(column = 3, row= 8,padx= 5, pady = 5, sticky='w')
+    labelVori_receta = Label(ConfHoja, text='Orientacion vertical:',font=(myFont,11), bg='#F08080')
+    labelVori_receta.grid(column = 2, row= 8,padx= 5, pady = 5, sticky='w')
+
+    labelHori_receta = Label(ConfHoja, text='Orientacion horizontal:',font=(myFont,11), bg='#F08080')
+    labelHori_receta.grid(column = 2, row= 10,padx= 5, pady = 5, sticky='w')
+
+    labelA_texto_receta = Label(ConfHoja, text='Ajustar Texto:',font=(myFont,11), bg='#F08080')
+    labelA_texto_receta.grid(column = 2, row= 12,padx= 5, pady = 5, sticky='w')
+
+    # BOXS NUMERO DE RECETA
+        # TEXT FIELD TAMANIO LETRA 
+    tmnLetra_receta = Entry(ConfHoja, text = '14' ,width=4) 
+    tmnLetra_receta.grid(column = 3, row= 6,padx= 5, pady = 5, sticky='w')
+        # BOX ORIENTACION VERTICAL DE TEXTO
+    ori_vert_receta = ['Arriba', 'Centro', 'Abajo']
+    boxOri_vert_receta = Combobox(ConfHoja, values=ori_vert_receta, width = 8)
+    boxOri_vert_receta.grid(column = 3, row= 8,padx= 5, pady = 5, sticky='w')
+        # BOX ORIENTACION HORIZONTAL  DE TEXTO
+    ori_hori_receta = ['Izquierda', 'Centro', 'Derecha']
+    boxOri_hori_receta = Combobox(ConfHoja, values=ori_hori_receta, width = 8)
+    boxOri_hori_receta.grid(column = 3, row= 10,padx= 5, pady = 5, sticky='w')
+        # BOX AJUSTAR TEXTO 
+    boxA_texto_receta = Checkbutton(ConfHoja, bg ='#F08080' , activebackground='#F08080', activeforeground="white")
+    boxA_texto_receta.grid(column = 3, row= 12,padx= 5, pady = 5, sticky='w')
 
     # ACA EMPIEZO A USAR XLSXWRITER
     workbook = xlsxwriter.Workbook('C:/Users/Pedri/Desktop/Prueba.xlsx') 
@@ -62,32 +78,32 @@ def abrir():
     # BOTON DE GUARDADO  
     def guardar (): # guarda en la lista y cierra la ventana
         boxtipoHoja.get() # Tipo de hoja
-        tmnLetra.get() # tamanio de letra
-        boxOri_vert.get() # orientacion vertical
-        boxOri_hori.get() # orientacion horizontal
+        tmnLetra_receta.get() # tamanio de letra
+        boxOri_vert_receta.get() # orientacion vertical
+        boxOri_hori_receta.get() # orientacion horizontal
 
         # Valida el campo de numero
         try:
-            float(tmnLetra.get())   
+            float(tmnLetra_receta.get())   
         except ValueError:    
             tkinter.messagebox.showerror('Error', 'Ingrese un numero')
 
         formatocelda = workbook.add_format()
         #Tamanio de letra 
-        formatocelda.set_font_size(tmnLetra.get())
+        formatocelda.set_font_size(tmnLetra_receta.get())
 
         #Orientaciones verticales
-        if boxOri_vert.get() == 'Arriba':
+        if boxOri_vert_receta.get() == 'Arriba':
             formatocelda.set_align('top')
-        elif  boxOri_vert.get() == 'Centro':
+        elif  boxOri_vert_receta.get() == 'Centro':
             formatocelda.set_align('vcenter')
         else:
             formatocelda.set_align('bottom')
 
         #Orientaciones horizontales
-        if boxOri_hori.get() == 'Derecha':
+        if boxOri_hori_receta.get() == 'Derecha':
             formatocelda.set_align('right')
-        elif  boxOri_hori.get() == 'Centro':
+        elif  boxOri_hori_receta.get() == 'Centro':
             formatocelda.set_align('center')
         else:
             formatocelda.set_align('left')
@@ -106,7 +122,7 @@ def abrir():
         worksheet.center_vertically()
 
         # cierra la ventana y guarda
-        if float(tmnLetra.get()):
+        if float(tmnLetra_receta.get()):
             tkinter.messagebox.showinfo('Guarado', 'La configuracion ha sido guardada correctamente')
             ConfHoja.destroy() # cierra la ventana y guarda
 
@@ -115,7 +131,7 @@ def abrir():
     pngGuardar = PhotoImage(file='C:/Users/Pedri/Desktop/Zurich/DLR/Iconos/discket.png') # Esto permite poner la imagen como boton
     savebtn = Button(ConfHoja, image = pngGuardar, command = guardar, bg='#F08080',activebackground= '#F08080' , borderwidth=0)
     #state=DISABLED Esto bloquea el boton, estaria bueno agregarlo cuando no hay nada tipeado
-    savebtn.grid(column = 70, row = 20,padx = 25, pady = 15)
+    #savebtn.grid(column = 20, row = 20,padx = 25, pady = 15) # Chequear lo de como se muestra
 
 btnOptions = Button(root, text = 'Configuracion de hoja', command = abrir)
 btnOptions.grid(column = 0, row = 0)
